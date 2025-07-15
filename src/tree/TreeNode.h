76d5767ff61ead7067d7ec3c573a8e486562d058
sisonjohneinstein <167601809+sisonjohneinstein@users.noreply.github.com>
2025-07-15 21:49:00 +0800

    Update TreeNode.h

---
+++ src/tree/TreeNode.h
@@ -0,0 +1,16 @@
+#ifndef TREE_NODE_H
+#define TREE_NODE_H
+
+struct TreeNode {
+    int data;
+    TreeNode* left;
+    TreeNode* right;
+
+    TreeNode(int val) {
+        data = val;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+
+#endif
