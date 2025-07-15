95c70f2cde9cd9426bfd5154b6de61181b3375d8
sisonjohneinstein <167601809+sisonjohneinstein@users.noreply.github.com>
2025-07-15 21:52:31 +0800

    Update BinaryTree.h

--- src/tree/BinaryTree.h
+++ src/tree/BinaryTree.h
@@ -1,4 +1,29 @@
 #ifndef BINARY_TREE_H
 #define BINARY_TREE_H
 
-#endif // BINARY_TREE_H
+#include "TreeNode.h"
+
+class BinaryTree {
+private:
+    TreeNode* root;
+    TreeNode* insert(TreeNode* node, int data);
+    TreeNode* remove(TreeNode* node, int data);
+    TreeNode* findMin(TreeNode* node);
+    void inOrder(TreeNode* node);
+    void preOrder(TreeNode* node);
+    void postOrder(TreeNode* node);
+    bool search(TreeNode* node, int key);
+
+public:
+    BinaryTree();
+    ~BinaryTree();
+    void insertNode(int data);
+    void deleteNode(int data);
+    void inOrderTraversal();
+    void preOrderTraversal();
+    void postOrderTraversal();
+    bool searchNode(int key);
+    void destroyTree(TreeNode* node);
+};
+
+#endif
