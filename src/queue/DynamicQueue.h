6a1381ee0aacba9b7b7222b1771dbd22d4d5757b
clarencebuising <20buisingcj@gmail.com>
2025-07-15 20:27:47 +0800

    Update DynamicQueue.h

--- src/queue/DynamicQueue.h
+++ src/queue/DynamicQueue.h
@@ -1,4 +1,24 @@
 #ifndef DYNAMIC_QUEUE_H
 #define DYNAMIC_QUEUE_H
 
-#endif // DYNAMIC_QUEUE_H
+class DynamicQueue {
+private:
+    struct Node {
+        int data;
+        Node* next;
+    };
+    Node* frontNode;
+    Node* rearNode;
+
+public:
+    DynamicQueue();
+    ~DynamicQueue();
+    bool isEmpty() const;
+    void enqueue(int value);
+    void dequeue();
+    int front() const;
+    int rear() const;
+    void display() const;
+};
+
+#endif
