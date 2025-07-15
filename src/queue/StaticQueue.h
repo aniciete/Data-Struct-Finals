747dac0d467f294c44dbe32c4bf4cd64164befe7
clarencebuising <20buisingcj@gmail.com>
2025-07-15 20:24:34 +0800

    Update StaticQueue.h

--- src/queue/StaticQueue.h
+++ src/queue/StaticQueue.h
@@ -1,4 +1,23 @@
 #ifndef STATIC_QUEUE_H
 #define STATIC_QUEUE_H
 
-#endif // STATIC_QUEUE_H
+class StaticQueue {
+private:
+    static const int MAX_SIZE = 100;
+    int data[MAX_SIZE];
+    int frontIndex;
+    int rearIndex;
+    int count;
+
+public:
+    StaticQueue();
+    bool isEmpty() const;
+    bool isFull() const;
+    void enqueue(int value);
+    void dequeue();
+    int front() const;
+    int rear() const;
+    void display() const;
+};
+
+#endif
