2186ab1cc6fb92084030a55243618e50f8b5c08a
eamanaloto0627 <202411359@fit.edu.ph>
2025-07-16 09:15:34 +0800

    Update Graph.h

--- src/graph/Graph.h
+++ src/graph/Graph.h
@@ -1,4 +1,22 @@
 #ifndef GRAPH_H
 #define GRAPH_H
 
-#endif // GRAPH_H
+#include <iostream>
+#include <map>
+#include <vector>
+#include <queue>
+
+class Graph {
+private:
+    std::map<int, std::vector<int>> adjList;
+
+public:
+    void addVertex(int vertex);
+    void addEdge(int vertex1, int vertex2);
+    void removeVertex(int vertex);
+    void removeEdge(int vertex1, int vertex2);
+    void display();
+    void traverse(int startVertex);
+};
+
+#endif
