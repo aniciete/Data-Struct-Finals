c9a3281be894995bf3642ca0c86018610c32e169
aceacebabyyyy <haroldacebaby@gmail.com>
2025-07-16 20:25:58 +0800

    Update SortingAlgorithms.h

--- src/sorting/SortingAlgorithms.h
+++ src/sorting/SortingAlgorithms.h
@@ -1,4 +1,10 @@
 #ifndef SORTING_ALGORITHMS_H
 #define SORTING_ALGORITHMS_H
 
-#endif // SORTING_ALGORITHMS_H
+#include <vector>
+
+void bubbleSort(std::vector<int>& arr);
+void mergeSort(std::vector<int>& arr, int left, int right);
+void quickSort(std::vector<int>& arr, int low, int high);
+
+#endif
